5f08de09c42a9b5e9784e873c5d9c0873a697a38
codingbowoo <codingbowoo@gmail.com>
2018-01-14 20:18:45 +0900

    add graph & modify setting

--- monitor/views.py
+++ monitor/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at monitos index.")
+    return render(request, 'monitor_temp.html', )
 
 
